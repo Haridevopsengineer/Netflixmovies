d60535e3392405ac8cf506e8d5381757f043832f
Hari Prasad <Hari@devops.com>
2022-09-04 10:46:14 +0530

    Netflix movies updated

---
+++ src/main/java/Netflix/App.java
@@ -0,0 +1,13 @@
+package Netflix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
